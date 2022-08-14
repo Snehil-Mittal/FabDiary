560ad4f2f4064dbf84c56487b6bebaa7265dd158
Snehil-Mittal <snehilmittal2001@gmail.com>
2022-08-14 11:03:04 +0530

    Formatting and Adding Usernames instead of ids

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,21 @@ const app = require('express')();
 
 const server = require('http').createServer(app);
 
-const io = require('socket.io')(server,{
-    cors: {
-        origins: "http://localhost:5000",
-        methods: ["GET", "POST"]
-    }});
-
-io.on("connection",(socket)=>{
-    console.log(socket);
-    console.log("Connection is started by nodemon...");
-
-    socket.on("chat" , (payload)=>{
-        console.log(payload);
-        io.emit("chat",payload);
-    })
+const io = require('socket.io')(server, {
+	cors: {
+		origins: 'http://localhost:5000',
+		methods: ['GET', 'POST'],
+	},
 });
 
-server.listen(5000, ()=> console.log("server is active..."));
+io.on('connection', (socket) => {
+	console.log(socket);
+	console.log('Connection is started by nodemon...');
+
+	socket.on('chat', (payload) => {
+		console.log(payload);
+		io.emit('chat', payload);
+	});
+});
+
+server.listen(5000, () => console.log('server is active...'));
